0f8da260f0a6855d7112ee70b66f12f20dc9bf35
Indiara Duarte <indiara96d@gmail.com>
2020-09-26 23:09:57 -0300

    Navigation app config

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container />
+  );
+};
+
+export default SignUp;
