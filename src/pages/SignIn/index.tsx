5652583bc87f5753aa4545cf3c188418a78fcea9
Indiara Duarte <indiara96d@gmail.com>
2020-09-27 21:56:08 -0300

    Creating Button and Input components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Container, Title } from './styles';
 import { Image } from 'react-native';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -9,6 +11,10 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu login</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha"/>
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
