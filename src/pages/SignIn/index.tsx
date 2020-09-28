f23ccfb33eb35f468efb4e0cbb31973ad237701f
Indiara Duarte <indiara96d@gmail.com>
2020-09-27 22:20:14 -0300

    Adding forgot password and create account links into signup screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Container, Title } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButtonText, CreateAccountButton } from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container >
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container >
+            <Image source={logoImg} />
 
-      <Title>Faça seu login</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha"/>
+            <View>
+              <Title>Faça seu login</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+            <Button onPress={() => { }}>Entrar</Button>
+
+            <ForgotPassword onPress={() => { }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => { }}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
